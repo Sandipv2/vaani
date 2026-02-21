1abfbd52168cb5699c0c94ffecdef997aceaacbd
sandipv2 <sandiipvarma@gmail.com>
2026-02-21 21:25:01 +0530

    add user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get('/', (req,res) => {
     res.send("Hari Bol")
 })
+
+// Routes import
+import userRouter from "./routes/user.route.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app }
