9819ff13bf9cbd999cac382a1a6fbd945b6e2671
sandipv2 <sandiipvarma@gmail.com>
2026-03-29 12:49:27 +0530

    feat: integrate NativeWind and update app configuration

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-700">
+      <Text className="text-red-400 bg-slate-900 p-3 text-center font-bold">Hello World</Text>
+    </View>
+  );
+}
