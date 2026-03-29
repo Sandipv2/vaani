a5f8a40e196c5538d60b66b0eacd7cae962635d0
sandipv2 <sandiipvarma@gmail.com>
2026-03-29 16:46:59 +0530

    Authentication UI done.

--- mobile/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-slate-700">
-      <Text className="text-red-400 bg-slate-900 p-3 text-center font-bold">Hello World</Text>
-    </View>
-  );
-}
