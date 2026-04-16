ccea6a0f4730cad03c9af73afeee7e11fae78f71
sandipv2 <sandiipvarma@gmail.com>
2026-04-16 19:57:53 +0530

    feat: implement notifications feature with fetching, displaying, and deleting notifications

--- backend/src/routes/notification.route.js
+++ backend/src/routes/notification.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { getNotifications, deleteNotificaton } from "../controllers/notification.controller.js";
 
 const router = Router();
 
-router.get("/", protectRoute, );
-router.get("/:notificationId", protectRoute, );
+router.get("/", protectRoute, getNotifications);
+router.delete("/:notificationId", protectRoute, deleteNotificaton);
 
 export default router;
