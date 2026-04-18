51a5658ab2cbbeeb0adfebca821c3ba9cc852b2e
sandipv2 <sandiipvarma@gmail.com>
2026-04-18 22:49:27 +0530

    fix: update user data retrieval in PostComposer to use currentUser hook

--- mobile/components/PostComposer.tsx
+++ mobile/components/PostComposer.tsx
@@ -1,9 +1,9 @@
 import { View, Text, Image, TextInput, TouchableOpacity, ScrollView, Modal, Pressable, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import { SelectedFile, useCreatePost } from '@/hooks/useCreatePost'
-import { useUser } from '@clerk/expo';
 import { Feather } from '@expo/vector-icons';
 import { useVideoPlayer, VideoView } from 'expo-video';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
 
 const MediaPreviewModal = ({
     media,
@@ -56,14 +56,14 @@ const PostComposer = () => {
         createPost
     } = useCreatePost();
 
-    const { user } = useUser();
+    const { currentUser } = useCurrentUser();
 
     return (
         <View className="border-b border-gray-100 p-4 bg-white">
             <MediaPreviewModal media={previewMedia} onClose={() => setPreviewMedia(null)} />
 
             <View className="flex-row">
-                <Image source={{ uri: user?.imageUrl }} className="w-12 h-12 rounded-full mr-3" />
+                <Image source={{ uri: currentUser?.profilePicture || "" }} className="w-12 h-12 rounded-full mr-3" />
                 <View className="flex-1">
                     <TextInput
                         className="text-gray-900 text-lg"
